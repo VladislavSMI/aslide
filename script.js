ad694a272260f13130d25a9297f09aa6ae137e7d
Kaver <69860591+VladislavSMI@users.noreply.github.com>
2020-10-27 16:04:33 +0100

    navbar changing collor on scroll

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const callOutBox = document.getElementById("callOutBox");
+const header = document.querySelector(".header");
 
 callOutBox.addEventListener("click", (e) => {
   if (e.target.classList.contains("question")) {
     e.target.nextElementSibling.classList.toggle("show");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  let top = window.scrollY;
+  if (top >= 50) {
+  header.classList.add("active");
+  } else {
+  header.classList.remove("active");
   }
 });
