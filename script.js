514c94637820c420e03d41617f432f0a1bb8320b
Kaver <69860591+VladislavSMI@users.noreply.github.com>
2020-10-29 15:29:36 +0100

    changes in scroll nav bar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const callOutBox = document.getElementById("callOutBox");
-const header = document.querySelector(".header");
+const navbar = document.querySelector(".navbar");
 
 callOutBox.addEventListener("click", (e) => {
   if (e.target.classList.contains("question")) {
@@ -9,9 +9,17 @@ callOutBox.addEventListener("click", (e) => {
 
 window.addEventListener("scroll", () => {
   let top = window.scrollY;
+  let w = window.outerWidth;
   if (top >= 50) {
-  header.classList.add("active");
+    navbar.classList.add("scrollNav");
   } else {
-  header.classList.remove("active");
+    navbar.classList.remove("scrollNav");
   }
+});
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
 });
