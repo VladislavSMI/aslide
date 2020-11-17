efcb9977ea466ef61d3a34270b4ef2eb33770423
Kaver <69860591+VladislavSMI@users.noreply.github.com>
2020-11-17 20:36:47 +0100

    JS changed div toggle working for the whole div

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const callOutBox = document.getElementById("callOutBox");
 const navbar = document.querySelector(".navbar");
 
-callOutBox.addEventListener("click", (e) => {
-  if (e.target.classList.contains("question")) {
-    e.target.nextElementSibling.classList.toggle("show");
-  }
-});
-
 window.addEventListener("scroll", () => {
   let top = window.scrollY;
   let w = window.outerWidth;
@@ -23,3 +17,20 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+callOutBox.addEventListener("click", (e) => {
+  var target = e.target;
+  var parent = target.parentElement;
+  
+  if (parent.classList.contains("question")) {
+    parent.nextElementSibling.classList.toggle("show");
+  } else if (target.classList.contains("question")) {
+    target.nextElementSibling.classList.toggle("show");
+  }
+});
+
+// callOutBox.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("question")) {
+//     e.target.nextElementSibling.classList.toggle("show");
+//   }
+// });
